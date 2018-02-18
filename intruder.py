3e7d676cdd11bdb81d8a2d1669c0c30fb8b4df39
rounakdatta12@gmail.com <rounakdatta12@gmail.com>
2018-02-18 11:53:00 +0530

    code fetch complete

--- intruder.py
+++ intruder.py
@@ -4,40 +4,34 @@ from bs4 import BeautifulSoup
 login_page = 'http://care.srmuniv.ac.in/ktrcsejava2/login_check.php'
 home_page = 'http://care.srmuniv.ac.in/ktrcsejava2/login/student/home.php'
 
-question = 'http://care.srmuniv.ac.in/ktrcsejava2/login/student/question.php'
-
-question_page = 'http://care.srmuniv.ac.in/ktrcsejava2/login/student/code/java/java.code.php?id=1&value='
-evaluation_url = 'http://care.srmuniv.ac.in/ktrcsejava2/login/student/code/java/code.evaluate.elab.php'
-
-get_code = 'http://care.srmuniv.ac.in/ktrcsejava2/login/student/code/code.get.php'
-
 payload = {
-	'uname': 'RA1611003010***',
-	'pass': 'password'
+	'uname': 'RA1611003010672',
+	'pass': 'mkizrely616'
 }
 
 with requests.Session() as s:
 
-	#log in with given creds
-	login = s.post(login_page, data=payload)
+	# login page
 
-	#navigate to the homepage
-	get_home_page = s.get(home_page)
+	s.post(login_page, data=payload)
 
-	#print(get_home_page.text)
 
-	questionp = s.get(question)
+	# home page
 
+	s.get(home_page)
+
+
+	# question page requests & responses
+
+	s.get('http://care.srmuniv.ac.in/ktrcsejava2/login/student/question.php')
 	s.post('http://care.srmuniv.ac.in/ktrcsejava2/login/student/home.helper.php', data={'text': 'JAVA'})
 	s.get('http://care.srmuniv.ac.in/ktrcsejava2/login/student/question.php')
-
-	#print(questionp.text)
 	s.get('http://care.srmuniv.ac.in/ktrcsejava2/login/student/question.list.js')
+	s.post('http://care.srmuniv.ac.in/ktrcsejava2/login/student/course.get.php', data={'q': 'SESSION'})
+	s.post('http://care.srmuniv.ac.in/ktrcsejava2/login/student/course.get.php', data={'q': 'VALUES'})
 
-	s.get('http://care.srmuniv.ac.in/ktrcsejava2/login/student/course.get.php')
-	s.get('http://care.srmuniv.ac.in/ktrcsejava2/login/student/course.get.php')
 
-	#print(w.text)
+	# individual question -> code page
 
 	s.get('http://care.srmuniv.ac.in/ktrcsejava2/login/student/code/java/java.code.php?id=1&value=0')
 	s.get('http://care.srmuniv.ac.in/ktrcsejava2/Code-mirror/lib/codemirror.js')
@@ -50,6 +44,10 @@ with requests.Session() as s:
 
 	g = s.get('http://care.srmuniv.ac.in/ktrcsejava2/login/student/code/code.get.php')
 	print(g.text)
+
+	s.get('http://care.srmuniv.ac.in/ktrcsejava2/login/student/code/java/java.code.php?id=1&value=1')
+	x = s.get('http://care.srmuniv.ac.in/ktrcsejava2/login/student/code/code.get.php')
+	print(x.text)
 
 	'''for i in range(0, 5):
 		present_question = question_page + str(i)
