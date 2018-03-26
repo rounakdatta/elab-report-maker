d9f83b5e556790eccf1f3401c7b8d796ad2a93df
harshithmohan <gharhsitmohan@ymail.com>
2018-03-26 23:23:44 +0530

    Fixed crash when question not allocated

--- elabreport.py
+++ elabreport.py
@@ -73,44 +73,49 @@ def gen_report(username, password, elabx):
 	
 			present_question = question_page + str(i)
 			s.get(present_question)
-			code = s.get(elab['url'] + 'login/student/code/code.get.php')
-	
-			if(code.text != ''):
-	
-				if(elab['key'] == 'daa'):
+
+			if(s.get(present_question).text.find('NOT ALLOCATED')==-1):
+				code = s.get(elab['url'] + 'login/student/code/code.get.php')			
+
+				if(code.text != ''):
 		
-						evaluate_payload_c = s.post(elab['url'] + 'login/student/code/' + elab['key'] + '/code.evaluate.elab.php', data={'code': code.text, 'input': '', 'language': 'c'})
-						evaluate_payload_cpp = s.post(elab['url'] + 'login/student/code/' + elab['key'] + '/code.evaluate.elab.php', data={'code': code.text, 'input': '', 'language': 'cpp'})
-						evaluate_payload_java = s.post(elab['url'] + 'login/student/code/' + elab['key'] + '/code.evaluate.elab.php', data={'code': code.text, 'input': '', 'language': 'java'})
-						evaluate_payload_python = s.post(elab['url'] + 'login/student/code/' + elab['key'] + '/code.evaluate.elab.php', data={'code': code.text, 'input': '', 'language': 'python'})
-	
-						if '100' in [evaluate_payload_c.text[-4:-1], evaluate_payload_cpp.text[-4:-1], evaluate_payload_java.text[-4:-1], evaluate_payload_python.text[-4:-1]]:
-							complete_percent = '100'
-						else:
-							complete_percent = '0'
-		
-				else:
-					evaluate_payload = s.post(elab['url'] + 'login/student/code/' + elab['key'] + '/code.evaluate.elab.php', data={'code': code.text, 'input': ''})
-					complete_percent = evaluate_payload.text[-4:-1]
-	
+					if(elab['key'] == 'daa'):
 			
-	
-				if(complete_percent == '100'):
+							evaluate_payload_c = s.post(elab['url'] + 'login/student/code/' + elab['key'] + '/code.evaluate.elab.php', data={'code': code.text, 'input': '', 'language': 'c'})
+							evaluate_payload_cpp = s.post(elab['url'] + 'login/student/code/' + elab['key'] + '/code.evaluate.elab.php', data={'code': code.text, 'input': '', 'language': 'cpp'})
+							evaluate_payload_java = s.post(elab['url'] + 'login/student/code/' + elab['key'] + '/code.evaluate.elab.php', data={'code': code.text, 'input': '', 'language': 'java'})
+							evaluate_payload_python = s.post(elab['url'] + 'login/student/code/' + elab['key'] + '/code.evaluate.elab.php', data={'code': code.text, 'input': '', 'language': 'python'})
 		
-					print(str(i) + ' : getting report')
-					file = s.get(elab['url'] + 'login/student/code/getReport.php')
-	
-					with open(payload['uname'] + '-' + str(i) + '.png', 'wb') as f:
-						f.write(file.content)
+							if '100' in [evaluate_payload_c.text[-4:-1], evaluate_payload_cpp.text[-4:-1], evaluate_payload_java.text[-4:-1], evaluate_payload_python.text[-4:-1]]:
+								complete_percent = '100'
+							else:
+								complete_percent = '0'
+			
+					else:
+						evaluate_payload = s.post(elab['url'] + 'login/student/code/' + elab['key'] + '/code.evaluate.elab.php', data={'code': code.text, 'input': ''})
+						complete_percent = evaluate_payload.text[-4:-1]
 		
-				else:
+				
 		
-					print(str(i) + ' : evaluation error : Couldn\'t get report')
-	
-			else:		
-				print(str(i) + ' : No code written')
-	
-	
+					if(complete_percent == '100'):
+			
+						print(str(i) + ' : getting report')
+						file = s.get(elab['url'] + 'login/student/code/getReport.php')
+		
+						with open(payload['uname'] + '-' + str(i) + '.png', 'wb') as f:
+							f.write(file.content)
+			
+					else:
+			
+						print(str(i) + ' : evaluation error : Couldn\'t get report')
+		
+				else:		
+					print(str(i) + ' : No code written')
+
+			else:
+				print(str(i) + ' : Question not allocated')
+
+
 		# put all the images to PDF
 	
 		filename = payload['uname'] + '-' + elabx.upper() + '.pdf'
